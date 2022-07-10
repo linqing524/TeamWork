22a2474d7af18a57a2fe9505aa66e5262d30a0c8
linqing524 <1942083894@qq.com>
2022-07-10 11:29:39 +0800

    yy

--- pro/src/main.js
+++ pro/src/main.js
@@ -3,8 +3,15 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+Vue.use(VueAxios, axios)
 Vue.config.productionTip = false
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import App from './App.vue';
 
+Vue.use(ElementUI);
 new Vue({
   router,
   store,
